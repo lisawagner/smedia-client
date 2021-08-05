9104f4a865fb75bf2090640f1309dbcab923589d
Lisa Wagner <lisawagner.online@gmail.com>
2021-08-05 10:05:11 -0700

    deployment update

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: REACT_APP_URI,
+  uri: process.env.REACT_APP_URI,
 });
 
 const authLink = setContext(() => {
